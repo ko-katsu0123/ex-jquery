ccb755b2cfdae59bc1b8eb0b0b79a88e44fb155e
koichirokatsuyama <koichiro.katsuyama@rakus-partners.co.jp>
2021-05-17 12:23:04 +0900

    演習2修正（要 別方法での作成）

--- jquery_excercise2.js
+++ jquery_excercise2.js
@@ -1,13 +1,19 @@
+// 方法としては非推奨
 'use strict';
 let textColor = "blue";
+
 $(function(){
     $("#name").css("color", textColor);
+});
 
+$(function(){
     $("#btn").on("click", function(){
-        if(textColor.equals("blue")){
+        if(textColor == "blue"){
             $("#name").css("color", "red");
+            textColor = "red";
         } else {
             $("#name").css("color", "blue");
+            textColor = "blue";
         }
     });
 });
